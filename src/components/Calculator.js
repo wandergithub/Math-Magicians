a4c140eee665b8f5d65fc4b163497350205dd4e3
wandergithub <wandergonzalez0@hotmail.com>
2022-05-24 13:41:00 -0400

    Set calculator buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Display from './Display';
+import Button from './Button';
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -11,6 +12,37 @@ class Calculator extends React.Component {
     return (
       <div className="calculatorContainer">
         <Display />
+        <div className="buttons-container">
+          <div className="row">
+            <Button value="AC" />
+            <Button value="+/-" />
+            <Button value="%" />
+            <Button value="÷" />
+          </div>
+          <div className="row">
+            <Button value="7" />
+            <Button value="8" />
+            <Button value="9" />
+            <Button value="x" />
+          </div>
+          <div className="row">
+            <Button value="4" />
+            <Button value="5" />
+            <Button value="6" />
+            <Button value="-" />
+          </div>
+          <div className="row">
+            <Button value="1" />
+            <Button value="2" />
+            <Button value="3" />
+            <Button value="+" />
+          </div>
+          <div className="row">
+            <Button value="0" />
+            <Button value="." />
+            <Button value="=" />
+          </div>
+        </div>
       </div>
     );
   }
