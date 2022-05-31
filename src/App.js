ac77f50b327f3a8d2392f7be090db583855857ac
wandergithub <wandergonzalez0@hotmail.com>
2022-05-31 17:24:25 -0400

    Set React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './App.css';
 
 function App() {
   return (
-    <Calculator />
+    <Routes>
+      <Route path="/" element={<Calculator />} />
+    </Routes>
   );
 }
 
