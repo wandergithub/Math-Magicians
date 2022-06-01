d7a3f252bf3af94ccc3be9bae25f65d599ffb9cc
wandergithub <wandergonzalez0@hotmail.com>
2022-06-01 13:38:49 -0400

    Start calculate testing, add 2 tests

---
+++ src/__tests__/calculate.test.js
@@ -0,0 +1,45 @@
+import calculate from '../logic/calculate.js';
+
+describe("test calculate basic operations", () => {
+  test("Operate = with 10 + 5 resulting on total 15", () => {
+    const obj = {
+      total: "10",
+      next: "5",
+      operation: '+',
+    }
+    const buttonName = '=';
+    const expectedResult = {
+      total: "15",
+      next: null,
+      operation: null,
+    };
+    let result = 0;
+
+    result = calculate(obj, buttonName);
+
+    expect(result.total).toBe(expectedResult.total);
+    expect(result.next).toBe(expectedResult.next);
+    expect(result.operation).toBe(expectedResult.operation);
+  });
+
+  test("Operate AC clear all", () => {
+    const obj = {
+      total: "10",
+      next: "5",
+      operation: '+',
+    }
+    const buttonName = 'AC';
+    const expectedResult = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    let result = 0;
+
+    result = calculate(obj, buttonName);
+
+    expect(result.total).toBe(expectedResult.total);
+    expect(result.next).toBe(expectedResult.next);
+    expect(result.operation).toBe(expectedResult.operation);
+  });
+});
